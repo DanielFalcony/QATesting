5fd7199c5d23007800d82b812cac87063d8e8fb9
danielfalcony <danielfalcony@gmail.com>
2024-05-25 20:11:50 +0300

    hw_3 Done

--- hw_3/conftest.py
+++ hw_3/conftest.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 import yaml
 import pytest
-from hw_3_checkers import checkout
+from hw_3_checkers import checkout, take_data
 
 with open('config.yaml') as f:
     data = yaml.safe_load(f)
@@ -12,6 +12,10 @@ with open('config.yaml') as f:
 
 @pytest.fixture()
 def make_folders():
+    """
+    fixture for making folders
+    :return:
+    """
     return checkout(
         f"mkdir {data['folder_original']} {data['folder_in']} {data['folder_in']} {data['folder_out']} {data['folder_ext']} {data['folder_ext2']}",
         "")
@@ -19,12 +23,20 @@ def make_folders():
 
 @pytest.fixture()
 def clear_folders():
+    """
+    fixture for clearing folders
+    :return:
+    """
     return checkout(
         f"rm -rf {data['folder_in']}/* {data['folder_out']}/* {data['folder_ext']}/* {data['folder_ext2']}/*", "")
 
 
 @pytest.fixture()
 def make_files():
+    """
+    fixture for making files
+    :return:
+    """
     list_of_files = []
     for i in range(data['files_count']):
         filename = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
@@ -36,6 +48,10 @@ def make_files():
 
 @pytest.fixture()
 def make_subfolder():
+    """
+    fixture for making subfolders
+    :return:
+    """
     test_file_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
     sub_folder_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
     if not checkout(f"cd {data['folder_in']}; mkdir {sub_folder_name}", ""):
@@ -50,13 +66,33 @@ def make_subfolder():
 
 @pytest.fixture(autouse=True)
 def show_time():
+    """
+    fixture for showing time
+    :return:
+    """
     print(f"Start time: {datetime.now().strftime('%m/%d/%Y %H:%M:%S.%f')}")
     yield print(f"Stop time: {datetime.now().strftime('%m/%d/%Y %H:%M:%S.%f')}")
 
 
 @pytest.fixture()
 def make_broke_file():
-    checkout(f"cd {data['folder_in']}; 7z a {data['folder_out']}/{data['arx_name']}", "Everything is Ok")
-    checkout(f"truncate -s 1 {data['folder_out']}/{data['arx_name']}.7z", "")
+    """
+    fixture for making broke files
+    :return:
+    """
+    checkout(f"cd {data['folder_in']}; 7z a -t{data['arx_type']} {data['folder_out']}/{data['arx_name']}", "Everything is Ok")
+    checkout(f"truncate -s 1 {data['folder_out']}/{data['arx_name']}.{data['arx_type']}", "")
     yield f"{data['arx_name']}"
-    checkout(f"rm -f {data['folder_out']}/{data['arx_name']}.7z", "")
+    checkout(f"rm -f {data['folder_out']}/{data['arx_name']}.{data['arx_type']}", "")
+
+
+@pytest.fixture()
+def stat_log():
+    """
+    fixture for making stats log in log file, which announced in config.yaml
+    :return:
+    """
+    with open(f"{data['log_txt']}", "a+", encoding="utf-8") as l:
+        log_string = (f"Время: {datetime.now().strftime('%H:%M:%S.%f')}, Кол-во файлов: {data['files_count']}, "
+                      f"Размер файлов: {data['bs']}, Загрузка процессора: {take_data('cat /proc/loadavg')} \n")
+        yield l.write(log_string)
--- hw_3/hw_3_negative.py
+++ hw_3/hw_3_negative.py
@@ -4,15 +4,33 @@ from conftest import data
 
 class TestNegative:
     def test_negative_step1(self, make_folders, make_broke_file):
-        # test neg 1
-        assert checkout_negative(f"cd {data['folder_out']}; 7z e {data['arx_name']}.7z -o{data['folder_ext']} -y",
-                                 "ERROR"), "test1 FAIL"
+        """
+        test Extract files from archive (without using directory names)
+        :param make_folders:
+        :param make_broke_file:
+        :return:
+        """
+        assert checkout_negative(
+            f"cd {data['folder_out']}; 7z e {data['arx_name']}.{data['arx_type']} -o{data['folder_ext']} -y",
+            "ERROR"), "test1 FAIL"
 
     def test_negative_step2(self, make_folders, make_broke_file):
-        # test neg 1
-        assert checkout_negative(f"cd {data['folder_out']}; 7z x {data['arx_name']}.7z -o{data['folder_ext']} -y",
-                                 "ERROR"), "test1 FAIL"
+        """
+        test eXtract files with full paths
+        :param make_folders:
+        :param make_broke_file:
+        :return:
+        """
+        assert checkout_negative(
+            f"cd {data['folder_out']}; 7z x {data['arx_name']}.{data['arx_type']} -o{data['folder_ext']} -y",
+            "ERROR"), "test1 FAIL"
 
     def test_negative_step3(self, make_folders, make_broke_file):
-        # test neg 2
-        assert checkout_negative(f"cd {data['folder_out']}; 7z t {data['arx_name']}.7z", "ERROR"), "test3 FAIL"
+        """
+        Test integrity of archive
+        :param make_folders:
+        :param make_broke_file:
+        :return:
+        """
+        assert checkout_negative(f"cd {data['folder_out']}; 7z t {data['arx_name']}.{data['arx_type']}",
+                                 "ERROR"), "test3 FAIL"
